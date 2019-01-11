5450cebb40f3db04ad2677d1fec88a99e2528042
minakshisadana <minakshi.sadana@convalidatech.com>
2019-01-11 18:17:40 +0530

    Push notification token

---
+++ app/src/main/java/com/convalida/ctpl_dt10/foodypos/Constants.java
@@ -0,0 +1,7 @@
+package com.convalida.ctpl_dt10.foodypos;
+
+public class Constants {
+    public static final String CHANNEL_ID="my_channel_01";
+    public static final String CHANNEL_NAME="FoodyPOS notification";
+    public static final String CHANNEL_DESCRIPTION="";
+}
